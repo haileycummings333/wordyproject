d9bb22fd6d6c40d6c1d4237f134f7dbd7e8edc71
haileycummings333 <haileycummings333@gmail.com>
2023-11-16 19:07:18 -0600

    update2 11/16

--- app/src/main/java/com/example/wordy/MainActivity.java
+++ app/src/main/java/com/example/wordy/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.wordy;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -8,23 +10,45 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.DatabaseError;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import androidx.annotation.NonNull;
+
 public class MainActivity extends AppCompatActivity {
 
-    private EditText[] guessInputs;
+    EditText[][] editTextArrays = new EditText[6][5]; // 6 rows, 5 columns
+
     private String targetWord;
-    private int attemptsLeft;
+    String guessingWord;
+    //private int attemptsLeft;
+    private List<String> targetWordLetters;
     EditText edt1, edt2, edt3, edt4, edt5, edt6, edt7, edt8, edt9, edt10,
             edt11, edt12, edt13, edt14, edt15, edt16, edt17, edt18, edt19,
             edt20, edt21, edt22, edt23, edt24, edt25, edt26, edt27,
             edt28, edt29, edt30;
+    Button addWordButton, restartButton, clearButton, submitButton, hardModeButton;
+    DatabaseReference wordsRef;
+    int currentRowIndex = 0;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        wordsRef = FirebaseDatabase.getInstance().getReference("words");
 
-        //initialize all the edittext
+        // Load a random word from Firebase as the target word, SHOULD BE RANDOM
+        loadRandomWord();
+
+        //initialize all the edittexts
         //row 1
         edt1 = findViewById(R.id.et_1);
         edt2 = findViewById(R.id.et_2);
@@ -62,75 +86,217 @@ public class MainActivity extends AppCompatActivity {
         edt29 = findViewById(R.id.et_29);
         edt30 = findViewById(R.id.et_30);
 
-        // Load a random word from Firebase as the target word, SHOULD BE RANDOM
-        loadRandomWord();
+        editTextArrays[0] = new EditText[]{edt1, edt2, edt3, edt4, edt5};
+        editTextArrays[1] = new EditText[]{edt6, edt7, edt8, edt9, edt10};
+        editTextArrays[2] = new EditText[]{edt11, edt12, edt13, edt14, edt15};
+        editTextArrays[3] = new EditText[]{edt16, edt17, edt18, edt19, edt20};
+        editTextArrays[4] = new EditText[]{edt21, edt22, edt23, edt24, edt25};
+        editTextArrays[5] = new EditText[]{edt26, edt27, edt28, edt29, edt30};
 
 
+        addWordButton = findViewById(R.id.addButton);
+        addWordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Words.class);
+                startActivity(intent);
+            }
+        });
         //initialize submit button
-        Button submitButton = findViewById(R.id.submitButton);
+        submitButton = findViewById(R.id.submitButton);
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                checkGuess();
+                checkGuess(editTextArrays[currentRowIndex]);
+
+//                if (edt30.length() == 1)
+//                    checkGuess(edt26, edt27, edt28, edt29, edt30);
+//                else if (edt25.length() == 1)
+//                    checkGuess(edt21, edt22, edt23, edt24, edt25);
+//                else if (edt20.length() == 1)
+//                    checkGuess(edt16, edt17, edt18, edt19, edt20);
+//                else if (edt15.length() == 1)
+//                    checkGuess(edt11, edt12, edt13, edt14, edt15);
+//                else if (edt10.length() == 1)
+//                    checkGuess(edt6, edt7, edt8, edt9, edt10);
+//                else if (edt5.length() == 1)
+//                    checkGuess(edt1, edt2, edt3, edt4, edt5);
+           }
+        });
+        //initialize clear button
+        clearButton = findViewById(R.id.clearButton);
+        clearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clearRow(currentRowIndex);
+        }});
+        //initialize hard mode button
+        hardModeButton = findViewById(R.id.hardModeButton);
+        hardModeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO: add hard mode stuff
             }
         });
-
-
         //initialize restart button
-        Button restartButton = findViewById(R.id.restartButton);
+        restartButton = findViewById(R.id.restartButton);
         restartButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //row 1 blank
-                edt1.setText("");
-                edt2.setText("");
-                edt3.setText("");
-                edt4.setText("");
-                edt5.setText("");
-                //row 2 blank
-                edt6.setText("");
-                edt7.setText("");
-                edt8.setText("");
-                edt9.setText("");
-                edt10.setText("");
-                //row 3 blank
-                edt11.setText("");
-                edt12.setText("");
-                edt13.setText("");
-                edt14.setText("");
-                edt15.setText("");
-                //row 4 blank
-                edt16.setText("");
-                edt17.setText("");
-                edt18.setText("");
-                edt19.setText("");
-                edt20.setText("");
-                //row 5 blank
-                edt21.setText("");
-                edt22.setText("");
-                edt23.setText("");
-                edt24.setText("");
-                edt25.setText("");
-                //row 6 blank
-                edt26.setText("");
-                edt27.setText("");
-                edt28.setText("");
-                edt29.setText("");
-                edt30.setText("");
+                restartGame();
             }
         });
-
-
-
     }
-
-
+    private void clearRow(int rowIndex) {
+        EditText[] currentRow = editTextArrays[rowIndex];
+        for (EditText editText : currentRow) {
+            editText.setText("");
+            editText.setBackgroundColor(Color.WHITE);
+            editText.setTextColor(Color.BLACK);
+        }
+    }
+    private void restartGame(){
+        //get a new word
+        loadRandomWord();
+        //clear all the rows
+        for(int i = 0; i<6; i++){
+            clearRow(i);
+        }
+    }
+    //load words from firebase into a list
     private void loadRandomWord() {
-        //
+        wordsRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                List<String> wordList = new ArrayList<>();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    String word = snapshot.getValue(String.class);
+                    if (word != null) {
+                        wordList.add(word);
+                    }
+                }
+                if (!wordList.isEmpty()) {
+                    // select a random word from the list
+                    targetWord = getRandomWord(wordList);
+                    // for hard mode, store the letters of the target word in a list
+                    targetWordLetters = Arrays.asList(targetWord.split(""));
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
+    }
+    //selects a random word from the world list
+    private String getRandomWord(List<String> wordList) {
+        if (wordList != null && !wordList.isEmpty()) {
+            Random random = new Random();
+            int randomIndex = random.nextInt(wordList.size());
+            return wordList.get(randomIndex);
+        } else {
+            //stops app if no words available
+            throw new IllegalStateException("No words available");
+        }
+    }
+    public void checkGuess(EditText[] editArray){
+        EditText et1 = editArray[0];
+        EditText et2 = editArray[1];
+        EditText et3 = editArray[2];
+        EditText et4 = editArray[3];
+        EditText et5 = editArray[4];
+        guessingWord = et1.getText().toString() +et2.getText().toString()+et3.getText().toString()
+                + et4.getText().toString() + et5.getText().toString();
+        //check if word is in database
+        checkWordAvailability(guessingWord);
+        //get char for each letter entered to compare to letters of target word
+        char editText1 = et1.getText().toString().toLowerCase().charAt(0);
+        char editText2 = et2.getText().toString().toLowerCase().charAt(0);
+        char editText3 = et3.getText().toString().toLowerCase().charAt(0);
+        char editText4 = et4.getText().toString().toLowerCase().charAt(0);
+        char editText5 = et5.getText().toString().toLowerCase().charAt(0);
+        //get char for each letter of target word to compare to guess
+        char l1 = targetWord.charAt(0);
+        char l2 = targetWord.charAt(1);
+        char l3 = targetWord.charAt(2);
+        char l4 = targetWord.charAt(3);
+        char l5 = targetWord.charAt(4);
+        // compares if letter a letter in word but in wrong spot, changes to yellow
+        if (editText1 == l2 || editText1 == l3 || editText1 == l4 || editText1 == l5) {
+            et1.setBackgroundColor(getResources().getColor(R.color.yellow)); // sets color to yellow if so
+        }
+        // compares if later entered in edt is equal to any of the other letters
+        if (editText2 == l1 || editText2 == l3 || editText2 == l4 || editText2 == l5) {
+            et2.setBackgroundColor(getResources().getColor(R.color.yellow)); // sets color to yellow if so
+        }
+        // compares if later entered in edt is equal to any of the other letters
+        if (editText3 == l1 || editText3 == l2 || editText3 == l4 || editText3 == l5) {
+            et3.setBackgroundColor(getResources().getColor(R.color.yellow)); // sets color to yellow if so
+        }
+        // compares if later entered in edt is equal to any of the other letters
+        if (editText4 == l1 || editText4 == l2 || editText4 == l3 || editText4 == l5) {
+            et4.setBackgroundColor(getResources().getColor(R.color.yellow)); // sets color to yellow if so
+        }
+        // compares if later entered in edt is equal to any of the other letters
+        if (editText5 == l1 || editText5 == l2 || editText5 == l3 || editText5 == l4) {
+            et5.setBackgroundColor(getResources().getColor(R.color.yellow)); // sets color to yellow if so
+        }
+        // checks if letter is equal to letter in word, changes color to green
+        if (editText1 == l1) {
+            et1.setBackgroundColor(getResources().getColor(R.color.green));
+        }
+        if (editText2 == l2) {
+            et2.setBackgroundColor(getResources().getColor(R.color.green));
+        }
+        if (editText3 == l3) {
+            et3.setBackgroundColor(getResources().getColor(R.color.green));
+        }
+        if (editText4 == l4) {
+            et4.setBackgroundColor(getResources().getColor(R.color.green));
+        }
+        if (editText5 == l5) {
+            et5.setBackgroundColor(getResources().getColor(R.color.green));
+        }
+        //checks if none of the letters are in the word, changes colors to gray
+        if (editText1 != l1 && editText1 != l2 && editText1 != l3 && editText1 != l4 && editText1 != l5) {
+            et1.setBackgroundColor(getResources().getColor(R.color.gray));
+        }
+        if (editText2 != l1 && editText2 != l2 && editText2 != l3 && editText2 != l4 && editText2 != l5) {
+            et2.setBackgroundColor(getResources().getColor(R.color.gray));
+        }
+        if (editText3 != l1 && editText3 != l2 && editText3 != l3 && editText3 != l4 && editText3 != l5) {
+            et3.setBackgroundColor(getResources().getColor(R.color.gray));
+        }
+        if (editText4 != l1 && editText4 != l2 && editText4 != l3 && editText4 != l4 && editText4 != l5) {
+            et4.setBackgroundColor(getResources().getColor(R.color.gray));
+        }
+        if (editText5 != l1 && editText5 != l2 && editText5 != l3 && editText5 != l4 && editText5 != l5) {
+            et5.setBackgroundColor(getResources().getColor(R.color.gray));
+        }
+        currentRowIndex++;
+        if (editText1 == l1 && editText2 == l2 && editText3 == l3 && editText4 == l4 && editText5 == l5) {
+            Toast.makeText(getApplicationContext(), "You have guessed the right word!", Toast.LENGTH_LONG).show();
+            restartGame();
+        }
     }
 
-    private void checkGuess() {
-        //
+    private void checkWordAvailability(String word){
+        wordsRef.orderByValue().equalTo(word).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                }
+                else {
+                    //when i use the currentRowIndex for clearRow it always clears the
+                    //row below, not sure why
+                    clearRow(currentRowIndex-1);
+                    Toast.makeText(MainActivity.this, "Word Not In Database", Toast.LENGTH_LONG).show();
+                    currentRowIndex--;
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // handle errors, if any
+            }
+        });
     }
-
 }
--- app/src/main/java/com/example/wordy/Words.java
+++ app/src/main/java/com/example/wordy/Words.java
@@ -1,19 +1,114 @@
 package com.example.wordy;
 
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 
-public class Words extends AppCompatActivity{
-    FirebaseDatabase db;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.content.Intent;
+
+
+public class Words extends AppCompatActivity {
+    FirebaseDatabase database;
+    EditText newWordText;
+    Button addButton, clearButton, backButton;
+    DatabaseReference wordsRef;
+
+
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        //initialize firebase database
+        database = FirebaseDatabase.getInstance();
+        wordsRef = FirebaseDatabase.getInstance().getReference("words");
+
+        //initialize stuff from xml
+        newWordText = findViewById(R.id.addWordet);
+        addButton = findViewById(R.id.addWordButton);
+        clearButton = findViewById(R.id.clearDBButton);
+        backButton = findViewById(R.id.returnButton);
+
+        //button listeners
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addWord(newWordText);
+            }
+        });
+
+        clearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                wordsRef.removeValue();
+            }
+
+        });
+
+        backButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Words.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
+    public void addWord(View view) {
+        String newWord = newWordText.getText().toString().toLowerCase(); //convert to lowercase for case-insensitive comparison
+
+        // check if the word is empty or not exactly 5 characters
+        if (newWord.length() != 5) {
+            Toast.makeText(this, "Word must be exactly 5 characters long.", Toast.LENGTH_LONG).show();
+            newWordText.setText("");
+            return;
+        }
+
+        // check if the word contains only letters
+        if (!newWord.matches("[a-z]+")) {
+            Toast.makeText(this, "Word must contain only letters.", Toast.LENGTH_LONG).show();
+            newWordText.setText("");
+            return;
+        }
+
+        // query to check if the word already exists in the database
+        wordsRef.orderByValue().equalTo(newWord).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    // word already exists in the database, don't add
+                    Toast.makeText(Words.this, "Word already exists in the database.", Toast.LENGTH_LONG).show();
+                    newWordText.setText("");
+                } else {
+                    // word doesn't already exist so add
+                    String key = wordsRef.push().getKey();
+                    if (key != null) {
+                        wordsRef.child(key).setValue(newWord);
+                        Toast.makeText(Words.this, "Word Added", Toast.LENGTH_LONG).show();
+                        newWordText.setText("");
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // handle errors, if any
+            }
+        });
+    }
 
 
 }
